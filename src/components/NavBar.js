7883c81b27e0863f5b557546b3ef89122b95776b
¨DevSeba78¨ <villarroelssebastian@gmail.com>
2021-08-27 00:28:25 -0300

    creo navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+function NavBar() {
+    return(
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><i class="fab fa-pagelines"> Ruca Design</i></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Productos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Servicios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contactos</a>
+          </li>     
+        </ul>
+      </div>
+    </div>
+  </nav>
+    );
+}
+
+export default NavBar;
