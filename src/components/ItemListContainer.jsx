a5da6305cde94ab2a58e42a00091b244b0039f7c
¨DevSeba78¨ <villarroelssebastian@gmail.com>
2021-09-07 16:49:43 -0300

    agrego funcion count a app

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,12 @@
+import ItemCount from "./ItemCount";
 
-
-const ItemListContainer = ({name}) => {
+const ItemListContainer = ({name, onAdd}) => {
+    
+    
     return (
         <>
             <p className="text-center mt-3">Bienvenido <strong>{name}</strong> a la app de Ruca </p>
+            <ItemCount stock={5} initial={1} onAdd={onAdd}/>
         </>
     )
 }
