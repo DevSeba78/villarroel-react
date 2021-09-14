627e6ffd91c399731e1bd55fd3bd918c3ca25609
¨DevSeba78¨ <villarroelssebastian@gmail.com>
2021-09-13 23:04:14 -0300

    Desafio 6

--- src/components/ItemList/Item.jsx
+++ src/components/ItemList/Item.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 
-const Item = () => {
+
+const Item = ({articulos}) => {
     return (
         <div>
-            
+            <div key={articulos.id} className="card w-25 mt-3 text-center">
+                <div className="card-header">{articulos.name}</div>
+                <div className="card-body">
+                    <img src={articulos.img} alt="fotos"/><br />
+                    {articulos.descripcion}</div>
+                <div className="card-footer">
+                    <button className="btn btn-outline-success">Detalles</button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,11 +2,12 @@ import ItemCount from "./ItemCount";
 import{ useState, useEffect} from "react";
 import ItemList from "./ItemList/ItemList";
 
+
 const Producto = [
-    { id: '1', name: 'ruca1', descripcion: 'mantel', stock: 8 },
-    { id: '2', name: 'ruca2', descripcion: 'bazar', stock: 2 },
-    { id: '3', name: 'ruca3', descripcion: 'deco', stock: 5 },
-    { id: '4', name: 'ruca4', descripcion: 'bazar', stock: 3 }
+    { id: '1', name: 'ruca1', descripcion: 'mantel', stock: 8, img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/357/585/products/c-bandeja-linea-farm1-ff30f4a83e4bdec4c116157608071720-240-0.jpg" },
+    { id: '2', name: 'ruca2', descripcion: 'bazar', stock: 2, img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/357/585/products/c-jarra-blana-esgrafiada-11-938f1325ca27f38b9416200640972411-240-0.jpg" },
+    { id: '3', name: 'ruca3', descripcion: 'deco', stock: 5, img:"https://d3ugyf2ht6aenh.cloudfront.net/stores/001/357/585/products/m-volados-gris-blanco-31-c397650e12b2e7c0d816173165003341-240-0.jpg" },
+    { id: '4', name: 'ruca4', descripcion: 'bazar', stock: 3, img:"https://d3ugyf2ht6aenh.cloudfront.net/stores/001/357/585/products/m-serigrafia-alamo-arena1-8b54fd37cd8c89139b16173132141470-240-0.jpg" }
   ];
  
   const promesa = new Promise((resolve, reject) => {//resolve(resuelvo) y reject(rechazado) son funciones clase 6 Promise
@@ -15,7 +16,7 @@ const Producto = [
         setTimeout(() => {
             resolve(Producto);//asigno el producto a resolve
             console.log(promesa);
-          }, 3000); //carga despues de 3seg
+          }, 2000); //carga despues de 3seg
     }else{
         reject("algo salio mal")
     }
@@ -28,15 +29,15 @@ const Producto = [
 
 const ItemListContainer = ({name, onAdd}) => {
     const [productos, setProductos] = useState([])//array vacio para luego setearlo en productos
-    //const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true)
     
     useEffect(() => {
         promesa.then(resu =>{
-            setProductos(resu)
+            setProductos(resu) //guardo el array que recibo y lo seteo en productos
            // console.log(resu);
         })
         .catch(err => console.error(err))
-        //.finally(()=>setLoading(false));
+        .finally(()=>setLoading(false));
                 
     }, [])
     
@@ -44,16 +45,9 @@ const ItemListContainer = ({name, onAdd}) => {
     return (
         <>
             <p className="text-center mt-3">Bienvenido <strong>{name}</strong> a la app de Ruca </p>
-            {/* {productos.map(item => <div key={item.id} className="card w-25 mt-3 text-center justify-content-center">
-                
-                <div className="card-header">{item.name}</div>
-                <div className="card-body">{item.descripcion}</div>
-                <div className="card-footer">
-                    <button className="btn-outline-success btn" onClick={()=>setLoading(!loading)}>Detalles</button>
-                </div>
-                
-            </div>)} */}
-            <ItemList items={Producto}/>
+             
+            {loading ?<h3>Cargando page....</h3>: <ItemList productos={productos}/>}
+            
             <ItemCount stock={5} initial={1} onAdd={onAdd}/>
         </>
     )
